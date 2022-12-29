c4c28b06db897bf0a409d2577128eb9122a23e44
Ivy070109 <Ivyaguilar07@gmail.com>
2022-12-29 19:45:00 -0300

    Base proyecto final

---
+++ JS/productos.js
@@ -0,0 +1,26 @@
+const productos = [
+    { id: 1, nombre: "Remera Lisa", precio: 2000, img: "../Imagenes/Remera.jpg", cantidad: 1 },
+    { id: 2, nombre: "Musculosa Butterfly", precio: 1500, img: "../Imagenes/Musculosa.jpg", cantidad: 1 },
+    { id: 3, nombre: "Top Pink", precio: 2000, img: "../Imagenes/Top.jpg", cantidad: 1 },
+    { id: 4, nombre: "Canguro Flower", precio: 4000, img: "../Imagenes/Canguro.jpg", cantidad: 1 },
+    { id: 5, nombre: "Buzo Basico", precio: 4500, img: "../Imagenes/Buzo.jpg", cantidad: 1 },
+    { id: 6, nombre: "Campera Biker", precio: 8000, img: "../Imagenes/Campera cuero.jpg", cantidad: 1 },
+    { id: 7, nombre: "Campera Jean", precio: 7000, img: "../Imagenes/Campera jean.jpg", cantidad: 1 },
+    { id: 8, nombre: "Skort", precio: 3000, img: "../Imagenes/Skort.jpg", cantidad: 1 },
+    { id: 9, nombre: "Short Jean", precio: 4000, img: "../Imagenes/Short.jpg", cantidad: 1 },
+    { id: 10, nombre: "Falda Tableada", precio: 3500, img: "../Imagenes/Pollera.jpg", cantidad: 1 },
+    { id: 11, nombre: "Pantalón Sastrero", precio: 6000, img: "../Imagenes/Pantalón Sastrero.jpg", cantidad: 1 },
+    { id: 12, nombre: "Pantalón de Cuero", precio: 6500, img: "../Imagenes/Pantalón cuero.jpg", cantidad: 1 },
+    { id: 13, nombre: "Pantalón de Lino", precio: 7000, img: "../Imagenes/Pantalón lino.jpg", cantidad: 1 },
+    { id: 14, nombre: "Jean Chupin", precio: 6000, img: "../Imagenes/Chupin.jpg", cantidad: 1 },
+    { id: 15, nombre: "Jean Wideleg", precio: 7000, img: "../Imagenes/Wideleg.jpg", cantidad: 1 },
+    { id: 16, nombre: "Vestido Rosa", precio: 5000, img: "../Imagenes/Vestido rosa.jpg", cantidad: 1 },
+    { id: 17, nombre: "Vestido Verano", precio: 5500, img: "../Imagenes/Vestidito.jpg", cantidad: 1 },
+    { id: 18, nombre: "Camisa", precio: 4500, img: "../Imagenes/Camisa.jpg", cantidad: 1 }, 
+    { id: 19, nombre: "Camisa Cuero", precio: 6000, img: "../Imagenes/Camisa cuero.jpg", cantidad: 1 },
+    { id: 20, nombre: "Blazer Negro", precio: 6000, img: "../Imagenes/Blazer.jpg", cantidad: 1 },
+    { id: 21, nombre: "Blazer Cuero", precio: 7000, img: "../Imagenes/Blazer cuero.jpg", cantidad: 1 },
+]
+
+
+//lista para arreglar: estilos, agregar filtros segun precio y categorias
